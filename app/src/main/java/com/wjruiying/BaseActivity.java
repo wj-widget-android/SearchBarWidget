168b5a37c93c02bfb05f80b5900bf751db51c727
纪昌帅 <jichangshuai@fulan.com.cn>
2020-11-22 16:30:37 +0800

    initialise

---
+++ app/src/main/java/com/wjruiying/BaseActivity.java
@@ -0,0 +1,55 @@
+package com.wjruiying;
+
+import android.os.Bundle;
+import android.view.MotionEvent;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        return super.dispatchTouchEvent(ev);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        return super.onTouchEvent(event);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
+}
---
+++ app/src/main/java/com/wjruiying/WJApplication.java
@@ -0,0 +1,10 @@
+package com.wjruiying;
+
+import android.app.Application;
+
+public class WJApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
